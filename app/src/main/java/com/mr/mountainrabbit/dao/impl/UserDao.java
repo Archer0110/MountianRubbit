8ddb2878397c9cfbdf16f4af74b43d7aef07b42d
BULING <2899353>
2017-02-24 09:16:51 +0800

    更改了UserDao里面的login方法

--- app/src/main/java/com/mr/mountainrabbit/dao/impl/UserDao.java
+++ app/src/main/java/com/mr/mountainrabbit/dao/impl/UserDao.java
@@ -3,6 +3,11 @@ package com.mr.mountainrabbit.dao.impl;
 import android.content.Context;
 
 import com.lidroid.xutils.BitmapUtils;
+import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.http.RequestParams;
+import com.lidroid.xutils.http.callback.RequestCallBack;
+import com.lidroid.xutils.http.client.HttpRequest;
+import com.mr.mountainrabbit.bean.Util;
 import com.mr.mountainrabbit.dao.IUserDao;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -22,4 +27,16 @@ public class UserDao implements IUserDao{
         BitmapUtils mBitmapUtils = new BitmapUtils(context);
         mBitmapUtils.display(imageView,uri);
     }
+
+    @Override
+    public void login(String userName, String userPass, RequestCallBack<String> callBack) {
+        HttpUtils httpUtils = new HttpUtils();
+        httpUtils.configTimeout(5000);
+        RequestParams params = new RequestParams();
+        params.addBodyParameter("username",userName);
+        params.addBodyParameter("password",userPass);
+        httpUtils.send(HttpRequest.HttpMethod.POST, Util.LOGINPATH,params,callBack);
+    }
+
+
 }

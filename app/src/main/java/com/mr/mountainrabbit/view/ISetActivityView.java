461dace4aa553c95ef40830055bd644f814f4d6c
BULING <2899353>
2017-02-27 16:17:07 +0800

    更改了新的东西

---
+++ app/src/main/java/com/mr/mountainrabbit/view/ISetActivityView.java
@@ -0,0 +1,15 @@
+package com.mr.mountainrabbit.view;
+
+import android.view.LayoutInflater;
+import android.view.View;
+
+/**
+ * Created by BULING on 2017/2/24.
+ */
+
+public interface ISetActivityView {
+    //获取物理返回键方法
+    public void getOnBackPress();
+
+
+}
